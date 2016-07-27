32ddfb280520705f6020d1c4513eb3a15c0e2e4d
Oleg Kozintsev <o.kozintsev@gmail.com>
2016-07-27 17:42:38 +0400

    add tests

---
+++ InteractiveEditor/MainWindow.py
@@ -0,0 +1,38 @@
+import sys
+from PyQt4 import QtGui, QtCore
+from PyQt4.QtGui import 
+
+class MainWindow(QtGui.QMainWindow):
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+
+        self.resize(350, 250)
+        self.setWindowTitle('mainwindow')
+
+        textEdit = QtGui.QTextEdit()
+        self.setCentralWidget(textEdit)
+
+        exit = QtGui.QAction(QtGui.QIcon('icons/exit.png'), 'Exit', self)
+        exit.setShortcut('Ctrl+Q')
+        exit.setStatusTip('Exit application')
+        self.connect(exit, QtCore.SIGNAL('triggered()'), QtCore.SLOT('close()'))
+
+        self.statusBar()
+
+        menubar = self.menuBar()
+        file = menubar.addMenu('&File')
+        file.addAction(exit)
+
+        toolbar = self.addToolBar('Exit')
+        toolbar.addAction(exit)
+        self.createDockWindows()
+        
+    def createDockWindows():
+        dock = QDockWidget('Customers', self)
+            
+
+
+app = QtGui.QApplication(sys.argv)
+main = MainWindow()
+main.show()
+sys.exit(app.exec_())
