92849b8559d6e21c1007385f286c85ccbceb4708
kozintsev <o.kozintsev@gmail.com>
2016-07-28 12:22:13 +0400

    research

--- InteractiveEditor/TestDock.py
+++ InteractiveEditor/TestDock.py
@@ -6,38 +6,53 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from spyderlib.widgets import internalshell
 
+
 class ManiWindow(QMainWindow):
-   def __init__(self, parent = None):
-      super(ManiWindow, self).__init__(parent)
-		
-      layout = QHBoxLayout()
-      bar = self.menuBar()
-      file = bar.addMenu("&File")
-      file.addAction("New")
-      # file.addAction("Save")
+    def __init__(self, parent=None):
+        super(ManiWindow, self).__init__(parent)
 
-      exit = QAction(QIcon('icons/exit.png'), '&Exit', self)
-      exit.setShortcut('Ctrl+Q')
-      exit.setStatusTip('Exit application')
-      self.connect(exit, SIGNAL('triggered()'), SLOT('close()'))
-      file.addAction(exit)
+        layout = QHBoxLayout()
+        bar = self.menuBar()
+        file = bar.addMenu("&File")
+        # file.addAction("Save")
 
-      self.statusBar()
-		
-      self.dock = QDockWidget("Python Shell", self)
-      self.pythonshell = internalshell.InternalShell(self.dock, namespace=globals(),commands=[])
-      self.dock.setWidget(self.pythonshell)
-      self.dock.setFloating(False)
-      self.setCentralWidget(QTextEdit())
-      self.addDockWidget(Qt.RightDockWidgetArea, self.dock)
-      self.setLayout(layout)
-      self.setWindowTitle("Dock demo")
-		
+        _exit = QAction(QIcon('icons/exit.png'), '&Exit', self)
+        _exit.setShortcut('Ctrl+Q')
+        _exit.setStatusTip('Exit application')
+        self.connect(_exit, SIGNAL('triggered()'), SLOT('close()'))
+        file.addAction(_exit)
+
+        _new = QAction(QIcon('icons/exit.png'), '&New', self)
+        _new.setStatusTip("New application")
+        self.connect(_new, SIGNAL("triggered()"), self.my_process)
+        file.addAction(_new)
+
+
+        self.statusBar()
+
+        self.dock = QDockWidget("Python Shell", self)
+        self.pythonshell = internalshell.InternalShell(self.dock, namespace=globals(), commands=[])
+        self.dock.setWidget(self.pythonshell)
+        self.dock.setFloating(False)
+        self.setCentralWidget(QTextEdit())
+        self.addDockWidget(Qt.RightDockWidgetArea, self.dock)
+        self.setLayout(layout)
+        self.setWindowTitle("Dock demo")
+
+    def my_process(self):
+        self.setWindowTitle("Hello!")
+        cmd = "print('hello')"
+        self.pythonshell.insert_text(cmd)
+        self.pythonshell.new_prompt('')
+        self.pythonshell.run_command(cmd)
+
+
 def main():
-   app = QApplication(sys.argv)
-   ex = ManiWindow()
-   ex.show()
-   sys.exit(app.exec_())
-	
+    app = QApplication(sys.argv)
+    ex = ManiWindow()
+    ex.show()
+    sys.exit(app.exec_())
+
+
 if __name__ == '__main__':
-   main()
+    main()
