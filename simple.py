5a1e3d1c616bbc96cc711f66eac9d2ddac7eb1e8
kozintsev <o.kozintsev@gmail.com>
2017-10-22 19:14:03 +0400

    fix and new simple

--- simple.py
+++ simple.py
@@ -5,13 +5,6 @@ from OCC.gp import gp_Pnt, gp_Dir, gp_Ax2
 display, start_display, add_menu, add_function_to_menu = init_display()
 my_box = BRepPrimAPI_MakeBox(10., 20., 30.).Shape()
 
-my_cyl = BRepPrimAPI_MakeCylinder(10., 20.).Shape()
-my_point = gp_Pnt(10., 10., 10.)
 
-pnt = gp_Pnt(0., 0., 0.)
-dr = gp_Dir(0., 1., 0.)
-ax = gp_Ax2(pnt, dr)
-my_cyl = BRepPrimAPI_MakeCylinder(ax, 10, 100)
-
-display.DisplayShape(my_cyl, update=True)
+display.DisplayShape(my_box, update=True)
 start_display()
